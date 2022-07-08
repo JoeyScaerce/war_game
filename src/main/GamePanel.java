13640dd1fe663f4dc165f72aabe8d8914a6f3b4d
JoeyScaerce <kenpalmerII@outlook.com>
2022-07-07 21:47:02 -0400

    working on player movement.

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -2,8 +2,9 @@ package main;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.Rectangle2D;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable{
 
     //Screen setting
     int originalTileSize = 16; // 16x16 tile
@@ -17,10 +18,77 @@ public class GamePanel extends JPanel {
     int screenWidth = tileSize*maxScreenCol;
     int screenHeight = tileSize*maxScreenRow;
 
-    public GamePanel() {
+    //frame rate (FPS)
+    int FPS = 60;
+    Rectangle playerContainer = new Rectangle();
+    //player
+    MouseHandler mouseH = new MouseHandler();
+    Rectangle2D r;
 
+    // set player position
+    int playerX = 100;
+    int playerY = 100;
+
+    Thread gameThread;
+
+    public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.cyan);
         this.setDoubleBuffered(true); // improve rendering performance
+        this.addMouseListener(mouseH);
+        this.addMouseMotionListener(mouseH);
+        this.setFocusable(true);
     }
+
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    //draw screen & character with updated information
+    @Override
+    public void run() {
+        double drawInterval = 1000000000/FPS;
+        double nextDrawTime = System.nanoTime() + drawInterval;
+
+        while (gameThread != null) {
+
+
+            update();
+            repaint(); // how you call paintComponent
+        }
+
+    }
+
+    public void update() {
+        if (mouseH.mouseOnePressed && r.contains(mouseH.getMousePosition())) {
+            playerX = mouseH.getX();
+            playerY = mouseH.getY();
+            System.out.println(r);
+            //System.out.println("mouse is pressed");
+        }
+    }
+
+
+    //Standard method to draw on jpanel
+    public void paintComponent(Graphics g) {
+        r = new Rectangle();
+
+        r.setRect(playerX, playerY, tileSize, tileSize);
+
+        super.paintComponent(g);
+        // parse to graphic 2d
+        // draws character currently
+        Graphics2D g2 = (Graphics2D)g;
+        g2.setColor(Color.red);
+        g2.fillRect(playerX, playerY, tileSize, tileSize);
+
+        g2.draw(r);
+        //playerContainer.setRect(playerX, playerY, tileSize, tileSize);
+
+
+        g2.dispose(); // saves memory by releasing system resources that it was using
+
+    }
+
 }
